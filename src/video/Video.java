0fb595732c2c017f14d18f1c582a4e17e980b982
nguimnac <nguimnac@S206-14.3il.fr>
2015-03-25 15:46:44 +0100

    2 init

--- src/video/Video.java
+++ src/video/Video.java
@@ -16,6 +16,8 @@ public class Video {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+        System.out.println("new");
     }
     
 }
